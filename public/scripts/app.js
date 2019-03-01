e8bfa8628e127f5e544d6c6563e56c5b09f0cd43
Ryan Andrew Sibat <ryanandrew.sibat@gmail.com>
2019-03-01 05:57:44 +0000

    created menu items via bootstrap, added remove button and function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -27,6 +27,26 @@ $(() => {
     window.localStorage.setItem("cart", JSON.stringify(cart))
   })
 
+  $("button.remove-from-cart").click((e) => {
+    const cart = JSON.parse(localStorage.getItem("cart")) || {}
+
+    const item = $(e.target)
+    const itemId = item.data("item-id")
+
+    if (cart[itemId]) {
+      cart[itemId].quantity -= 1
+      console.log('removed...')
+    } else {
+      cart[itemId] = {
+        quantity: 1,
+        itemName: item.data("item-name"),
+        price: item.data("item-price")
+      }
+    }
+
+    window.localStorage.setItem("cart", JSON.stringify(cart))
+  })
+
 
   const cart = JSON.parse(localStorage.getItem("cart")) || {}
 
